4c0e2e4c6d55e3d1bea5c4cf6cbcf2c6811abcd5
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2023-09-26 17:29:15 +0700

    feature create todo

--- controllers/todo-controller.js
+++ controllers/todo-controller.js
@@ -1,3 +1,21 @@
-exports.createTodo = (req, res, next) => {
-  res.status(201).json({ message: 'created' });
+const prisma = require('../models/prisma');
+
+exports.createTodo = async (req, res, next) => {
+  try {
+    const { title, completed, dueDate } = req.body;
+    // VALIDATE 1.Manual 2.Validation library eg. Joi, Yup, Zod
+    await prisma.todo.create({
+      data: {
+        title,
+        completed,
+        dueDate,
+        user: {
+          connect: req.user
+        }
+      }
+    });
+    res.status(201).json({ message: 'created' });
+  } catch (err) {
+    next(err);
+  }
 };
--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
+const prisma = require('../models/prisma');
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   try {
     const authorization = req.headers.authorization;
 
@@ -14,6 +15,17 @@ module.exports = (req, res, next) => {
     const token = authorization.split(' ')[1];
 
     const payload = jwt.verify(token, process.env.JWT_SECRET_KEY || 'very_secret');
+    const user = await prisma.user.findUnique({
+      where: {
+        id: payload.id
+      }
+    });
+
+    if (!user) {
+      return res.status(401).json({ message: 'unauthenticated' });
+    }
+
+    req.user = user;
     next();
   } catch (err) {
     next(err);
