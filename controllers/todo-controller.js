399558a2d60781ee53c62db75e58d847a06dafba
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2023-09-28 10:23:19 +0700

    feature update function create todo

--- controllers/todo-controller.js
+++ controllers/todo-controller.js
@@ -4,7 +4,9 @@ exports.createTodo = async (req, res, next) => {
   try {
     const { title, completed, dueDate } = req.body;
     // VALIDATE 1.Manual 2.Validation library eg. Joi, Yup, Zod
-    await prisma.todo.create({
+    // {id: '7de5a1b6-8cc3-4f91-8030-b02184bcd891', title: 'Swimming',
+    // completed:false, dueDate: null, userId: '63a39a13-45ec-494d-9267-21894ba8877c'}
+    const todo = await prisma.todo.create({
       data: {
         title,
         completed,
@@ -14,7 +16,7 @@ exports.createTodo = async (req, res, next) => {
         }
       }
     });
-    res.status(201).json({ message: 'created' });
+    res.status(201).json({ message: 'created', todo });
   } catch (err) {
     next(err);
   }
